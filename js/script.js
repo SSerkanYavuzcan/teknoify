ba54be7685abfcf940f10c242c9abc3b3477046d
SSerkanYavuzcan <sserkanyavuzcan99@gmail.com>
2026-05-06 21:32:42 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC1Id7kdU23_A7fEO1eDna0HKprvIM30E8",
+    apiKey: "AIzaSyC5Unp9aCYhY1aj2pQqvHeS7tkFOpiqN4E",
     authDomain: "teknoify-9449c.firebaseapp.com",
     projectId: "teknoify-9449c",
     storageBucket: "teknoify-9449c.firebasestorage.app",
