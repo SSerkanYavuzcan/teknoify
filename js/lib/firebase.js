e8e178e0e3b44948d5b08e92c6ab2b5b01ec2930
SSerkanYavuzcan <sserkanyavuzcan99@gmail.com>
2026-05-06 21:20:44 +0300

    Update firebase.js

--- js/lib/firebase.js
+++ js/lib/firebase.js
@@ -12,8 +12,9 @@ const firebaseConfig = {
   measurementId: "G-1DZKJE7BXE"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 export default app;
